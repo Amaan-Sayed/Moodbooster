c470bc88ccc092bb69e7d814b22162c8ce837552
Amaan Sayed <Amaan913@gmail.com>
2020-12-13 03:07:22 -0700

    New Baseline!

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
 
-const Main = () => {
-  const [videoId, setVideoId] = useState("");
-  // const [queryNum, setQueryNum] = useState(1);
+// Custom hook to persist data
+const useStickyState = (defaultValue, key) => {
+  const [value, setValue] = useState(() => {
+    const stickyValue = window.localStorage.getItem(key);
+    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+  });
 
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+};
+
+const Main = () => {
+  // State
+  const [videoId, setVideoId] = useStickyState("", "videoId");
+  const [numLeft, setNumLeft] = useStickyState(2, "numLeft");
+
+  // Constants
+  const API_KEY = process.env.REACT_APP_API_KEY;
   const CHANNEL_ID = "UCmh5gdwCx6lN7gEC20leNVA";
   const RESULT = 50;
   const ORDER = [
@@ -16,24 +32,32 @@ const Main = () => {
   ];
   const random = Math.floor(Math.random() * ORDER.length);
   const randomVidNum = Math.floor(Math.random() * 50);
-
-  const URL = `https://www.googleapis.com/youtube/v3/search?key=${
-    process.env.REACT_APP_GOOGLE_API_KEY
-  }&channelId=${CHANNEL_ID}&part=snippet,id&order=${
+  const URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=${
     ORDER[random]
   }&maxResults=${RESULT}`;
 
-  useEffect(() => {
-    // Queries a video from the Youtube API & updates the state
+  // Copy this URL for better customization
+  // "https://www.youtube.com/embed/?rel=0&amp;autoplay=1&amp;enablejsapi=1&amp;controls=1&amp;disablekb=0&amp;egm=0&amp;iv_load_policy=3&amp;showsearch=0&amp;showinfo=0&amp;fs=1&amp;hd=1&amp;modestbranding=1&amp;autohide=1&amp;playsinline=1&amp;origin=https%3A%2F%2Fgivemearandomdaviddobrikvlog.com&amp;widgetid=1"
+
+  // David Vlog Query
+  const query = () => {
     fetch(URL)
       .then((response) => response.json())
       .then((result) => {
+        console.log("hi");
         const VIDEO_RESULT = result.items[randomVidNum].id.videoId;
         setVideoId(`https://www.youtube.com/embed/${VIDEO_RESULT}`);
       })
       .catch((error) => console.error(error));
+  };
+
+  // Initial query if there's no previous video
+  useEffect(() => {
+    videoId === "" && query();
   }, []);
-  // }, [queryNum]);
+
+  // console.log(numLeft);
+  // window.localStorage.clear();
 
   return (
     <div className="main">
@@ -45,7 +69,6 @@ const Main = () => {
         src={videoId}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        // src="https://www.youtube.com/embed/?rel=0&amp;autoplay=1&amp;enablejsapi=1&amp;controls=1&amp;disablekb=0&amp;egm=0&amp;iv_load_policy=3&amp;showsearch=0&amp;showinfo=0&amp;fs=1&amp;hd=1&amp;modestbranding=1&amp;autohide=1&amp;playsinline=1&amp;origin=https%3A%2F%2Fgivemearandomdaviddobrikvlog.com&amp;widgetid=1"
         allowFullScreen
       />
 
@@ -53,11 +76,15 @@ const Main = () => {
       <div>
         <button
           className="button"
-          // onClick={() => setQueryNum(queryNum + 1)}
+          onClick={() => numLeft > 0 && (setNumLeft(numLeft - 1), query())}
         >
           New Vlog!
         </button>
-        <p className="amount">3 left today</p>
+        <button className="button" onClick={() => (setNumLeft(2), query())}>
+          Reset
+        </button>
+
+        <p className="amount">{numLeft} left today</p>
       </div>
     </div>
   );
